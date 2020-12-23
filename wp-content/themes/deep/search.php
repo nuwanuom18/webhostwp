9c4fa7635ae1ea4d2053d043139d343e87cd137b
nuwanuom18 <nuwan.18@cse.mrt.ac.lk>
2020-12-23 15:43:30 +0530

    update theme

---
+++ wp-content/themes/deep/search.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The template for displaying search results pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
+ *
+ * @package deep
+ */
+
+get_header();
+
+/**
+ * The function is located in the following path
+ * deep/inc/class-deep-theme-init.php
+ */
+do_action( 'deep_theme_search' );
+
+get_sidebar();
+get_footer();
---
+++ wp-content/themes/deep/single.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package deep
+ */
+
+get_header();
+
+/**
+ * The function is located in the following path
+ * deep/inc/class-deep-theme-init.php
+ */
+do_action( 'deep_theme_single' );
+
+get_sidebar();
+get_footer();
